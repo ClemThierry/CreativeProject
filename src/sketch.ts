884a0e60613da079bbbe529afc2815ae0b5ff98c
ClemThierry <49915513+ClemThierry@users.noreply.github.com>
2021-05-10 21:32:41 +0200

    Update sketch.ts

--- src/sketch.ts
+++ src/sketch.ts
@@ -10,36 +10,174 @@ gui.add(params, "Download_Image")
 
 // The SketchRNN model
 let model;
+const modelsToDraw = [
+  'alarm_clock',
+  'ambulance',
+  'angel',
+  'ant',
+  'antyoga',
+  'backpack',
+  'barn',
+  'basket',
+  'bear',
+  'bee',
+  'beeflower',
+  'bicycle',
+  'bird',
+  'book',
+  'brain',
+  'bridge',
+  'bulldozer',
+  'bus',
+  'butterfly',
+  'cactus',
+  'calendar',
+  'castle',
+  'cat',
+  'catbus',
+  'catpig',
+  'chair',
+  'couch',
+  'crab',
+  'crabchair',
+  'crabrabbitfacepig',
+  'cruise_ship',
+  'diving_board',
+  'dog',
+  'dogbunny',
+  'dolphin',
+  'duck',
+  'elephant',
+  'elephantpig',
+  'eye',
+  'face',
+  'fan',
+  'fire_hydrant',
+  'firetruck',
+  'flamingo',
+  'flower',
+  'floweryoga',
+  'frog',
+  'frogsofa',
+  'garden',
+  'hand',
+  'hedgeberry',
+  'hedgehog',
+  'helicopter',
+  'kangaroo',
+  'key',
+  'lantern',
+  'lighthouse',
+  'lion',
+  'lionsheep',
+  'lobster',
+  'map',
+  'mermaid',
+  'monapassport',
+  'monkey',
+  'mosquito',
+  'octopus',
+  'owl',
+  'paintbrush',
+  'palm_tree',
+  'parrot',
+  'passport',
+  'peas',
+  'penguin',
+  'pig',
+  'pigsheep',
+  'pineapple',
+  'pool',
+  'postcard',
+  'power_outlet',
+  'rabbit',
+  'rabbitturtle',
+  'radio',
+  'radioface',
+  'rain',
+  'rhinoceros',
+  'rifle',
+  'roller_coaster',
+  'sandwich',
+  'scorpion',
+  'sea_turtle',
+  'sheep',
+  'skull',
+  'snail',
+  'snowflake',
+  'speedboat',
+  'spider',
+  'squirrel',
+  'steak',
+  'stove',
+  'strawberry',
+  'swan',
+  'swing_set',
+  'the_mona_lisa',
+  'tiger',
+  'toothbrush',
+  'toothpaste',
+  'tractor',
+  'trombone',
+  'truck',
+  'whale',
+  'windmill',
+  'yoga',
+  'yogabicycle',
+  'everything',
+];
+let object = modelsToDraw[Math.floor(Math.random() * modelsToDraw.length)];
+
 // Start by drawing
 let previousPen = "down";
 // Current location of drawing
 let x, y;
 // The current "stroke" of the drawing
 let strokePath;
-let seedStrokes = [];
-let canvas;
 
-// -------------------
-//       Drawing
-// -------------------
+function pickRandomDrawing() {
+  object = modelsToDraw[Math.floor(Math.random() * modelsToDraw.length)];
+  return object;
+}
+
+// For when SketchRNN is fixed
+function preload() {
+  // See a list of all supported models: https://github.com/ml5js/ml5-library/blob/master/src/SketchRNN/models.js
+  model = ml5.sketchRNN(object);
+}
+
+console.log('ml5 version:', ml5.version);
+
+function setup() {
+  createCanvas(640, 480);
+  background(220);
+
+  // Button to reset drawing
+  const button = createButton("Replay");
+  button.mousePressed(startDrawing);
+
+  // run sketchRNN
+  startDrawing();
+}
+
+function modelReady() {
+  console.log("model loaded");
+  startDrawing();
+}
+
+// Reset the drawing
+function startDrawing() {
+  background(220);
+  // Start in the middle
+  x = width / 2;
+  y = height / 2;
+  model.reset();
+  // Generate the first stroke path
+  model.generate(gotStroke);
+  console.log(object);
+}
 
 function draw() {
-      // If the mosue is pressed capture the user strokes
-  if (mouseIsPressed) {
-    // Draw line
-    stroke(0);
-    strokeWeight(3.0);
-    line(pmouseX, pmouseY, mouseX, mouseY);
-    // Create a "stroke path" with dx, dy, and pen
-    const userStroke = {
-      dx: mouseX - pmouseX,
-      dy: mouseY - pmouseY,
-      pen: "down",
-    };
-    // Add to the array
-    seedStrokes.push(userStroke);
-  }
-
   // If something new to draw
   if (strokePath) {
     // If the pen is down, draw a line
@@ -62,53 +200,26 @@ function draw() {
   }
 }
 
-// -------------------
-//    Initialization
-// -------------------
-
-function setup() {
-    canvas = p6_CreateCanvas();
-
-    background(220);
-    // Load the model
-    // See a list of all supported models: https://github.com/ml5js/ml5-library/blob/master/src/SketchRNN/models.js
-    model = ml5.sketchRNN("cat", modelReady);
-  
-    // Button to start drawing
-    const button = select("#clear");
-    button.mousePressed(clearDrawing);
-}
-
-function windowResized() {
-    p6_ResizeCanvas()
-}
-
-// The model is ready
-function modelReady() {
-    // sketchRNN will begin when the mouse is released
-    canvas.mouseReleased(startSketchRNN);
-    select("#status").html("model ready - sketchRNN will begin after you draw with the mouse");
-  }
-
-// Reset the drawing
-function clearDrawing() {
-    background(220);
-    // clear seed strokes
-    seedStrokes = [];
-    // Reset model
-    model.reset();
-  }
-
-  // sketchRNN takes over
-function startSketchRNN() {
-    // Start where the mouse left off
-    x = mouseX;
-    y = mouseY;
-    // Generate with the seedStrokes
-    model.generate(seedStrokes, gotStroke);
-  }
-
-  // A new stroke path
+// A new stroke path
 function gotStroke(err, s) {
-    strokePath = s;
+  strokePath = s;
+}
+
+let scorePlayer = 0:
+  let scoreAI = 0;
+
+document.querySelector("#validation").addEventListener("click", function (event) {
+  event.preventDefault();
+  let word = document.querySelector("#model").value;
+  let score = document.querySelector("#scorePlayer");
+  if (object == word) {
+    scorePlayer += 1;
+    score.innerHTML = "You : " + scorePlayer;
+
+    //Créer une fonction pour changer le dessin (object)
+    //changement de dessin    
+    model = ml5.sketchRNN(pickRandomDrawing());
   }
+
+}, false);
+
