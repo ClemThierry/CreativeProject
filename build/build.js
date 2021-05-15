3308aab482a40b0f257dc1c5edbda407b9d0f949
Théo <theo.clement.p@gmail.com>
2021-05-15 15:57:28 +0200

    Je sais pas ce qui a changé mdr

--- build/build.js
+++ build/build.js
@@ -4,23 +4,154 @@ var params = {
 };
 gui.add(params, "Download_Image");
 var model;
+var modelsToDraw = [
+    'alarm_clock',
+    'ambulance',
+    'angel',
+    'ant',
+    'antyoga',
+    'backpack',
+    'barn',
+    'basket',
+    'bear',
+    'bee',
+    'beeflower',
+    'bicycle',
+    'bird',
+    'book',
+    'brain',
+    'bridge',
+    'bulldozer',
+    'bus',
+    'butterfly',
+    'cactus',
+    'calendar',
+    'castle',
+    'cat',
+    'catbus',
+    'catpig',
+    'chair',
+    'couch',
+    'crab',
+    'crabchair',
+    'crabrabbitfacepig',
+    'cruise_ship',
+    'diving_board',
+    'dog',
+    'dogbunny',
+    'dolphin',
+    'duck',
+    'elephant',
+    'elephantpig',
+    'eye',
+    'face',
+    'fan',
+    'fire_hydrant',
+    'firetruck',
+    'flamingo',
+    'flower',
+    'floweryoga',
+    'frog',
+    'frogsofa',
+    'garden',
+    'hand',
+    'hedgeberry',
+    'hedgehog',
+    'helicopter',
+    'kangaroo',
+    'key',
+    'lantern',
+    'lighthouse',
+    'lion',
+    'lionsheep',
+    'lobster',
+    'map',
+    'mermaid',
+    'monapassport',
+    'monkey',
+    'mosquito',
+    'octopus',
+    'owl',
+    'paintbrush',
+    'palm_tree',
+    'parrot',
+    'passport',
+    'peas',
+    'penguin',
+    'pig',
+    'pigsheep',
+    'pineapple',
+    'pool',
+    'postcard',
+    'power_outlet',
+    'rabbit',
+    'rabbitturtle',
+    'radio',
+    'radioface',
+    'rain',
+    'rhinoceros',
+    'rifle',
+    'roller_coaster',
+    'sandwich',
+    'scorpion',
+    'sea_turtle',
+    'sheep',
+    'skull',
+    'snail',
+    'snowflake',
+    'speedboat',
+    'spider',
+    'squirrel',
+    'steak',
+    'stove',
+    'strawberry',
+    'swan',
+    'swing_set',
+    'the_mona_lisa',
+    'tiger',
+    'toothbrush',
+    'toothpaste',
+    'tractor',
+    'trombone',
+    'truck',
+    'whale',
+    'windmill',
+    'yoga',
+    'yogabicycle',
+    'everything',
+];
+var object = modelsToDraw[Math.floor(Math.random() * modelsToDraw.length)];
 var previousPen = "down";
 var x, y;
 var strokePath;
-var seedStrokes = [];
-var canvas;
+function pickRandomDrawing() {
+    object = modelsToDraw[Math.floor(Math.random() * modelsToDraw.length)];
+    return object;
+}
+function preload() {
+    model = ml5.sketchRNN(object);
+}
+console.log('ml5 version:', ml5.version);
+function setup() {
+    createCanvas(640, 480);
+    background(220);
+    var button = createButton("Replay");
+    button.mousePressed(startDrawing);
+    startDrawing();
+}
+function modelReady() {
+    console.log("model loaded");
+    startDrawing();
+}
+function startDrawing() {
+    background(220);
+    x = width / 2;
+    y = height / 2;
+    model.reset();
+    model.generate(gotStroke);
+    console.log(object);
+}
 function draw() {
-    if (mouseIsPressed) {
-        stroke(0);
-        strokeWeight(3.0);
-        line(pmouseX, pmouseY, mouseX, mouseY);
-        var userStroke = {
-            dx: mouseX - pmouseX,
-            dy: mouseY - pmouseY,
-            pen: "down",
-        };
-        seedStrokes.push(userStroke);
-    }
     if (strokePath) {
         if (previousPen === "down") {
             stroke(0);
@@ -36,33 +167,20 @@ function draw() {
         }
     }
 }
-function setup() {
-    canvas = p6_CreateCanvas();
-    background(220);
-    model = ml5.sketchRNN("cat", modelReady);
-    var button = select("#clear");
-    button.mousePressed(clearDrawing);
-}
-function windowResized() {
-    p6_ResizeCanvas();
-}
-function modelReady() {
-    canvas.mouseReleased(startSketchRNN);
-    select("#status").html("model ready - sketchRNN will begin after you draw with the mouse");
-}
-function clearDrawing() {
-    background(220);
-    seedStrokes = [];
-    model.reset();
-}
-function startSketchRNN() {
-    x = mouseX;
-    y = mouseY;
-    model.generate(seedStrokes, gotStroke);
-}
 function gotStroke(err, s) {
     strokePath = s;
 }
+var scorePlayer = 0, let, scoreAI = 0;
+document.querySelector("#validation").addEventListener("click", function (event) {
+    event.preventDefault();
+    var word = document.querySelector("#model").value;
+    var score = document.querySelector("#scorePlayer");
+    if (object == word) {
+        scorePlayer += 1;
+        score.innerHTML = "You : " + scorePlayer;
+        model = ml5.sketchRNN(pickRandomDrawing());
+    }
+}, false);
 var __ASPECT_RATIO = 1;
 var __MARGIN_SIZE = 25;
 function __desiredCanvasWidth() {
